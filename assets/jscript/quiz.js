7d7cc3be28657322f2502d6563ccd2b268ebbe33
Mikhail <mikhail86@icloud.com>
2022-04-04 21:51:27 +0100

    decleared global variables to target sections and buttons by classes and id's

---
+++ assets/jscript/quiz.js
@@ -0,0 +1,13 @@
+// declaring global variables by targeting sections and button
+const userName = document.getElementById("username")
+const home = document.getElementById("home");
+const quiz = document.getElementById("quiz");
+const results = document.getElementById("resultsBoard");
+const playBtn = document.querySelector(".play-btn");
+const quizBtn = document.querySelector(".next-btn");
+const homeBtn = document.querySelectorAll(".home-btn");
+
+const questionBoard = document.getElementById("question");
+const usersAnswer = document.querySelectorAll(".answer");
+
+
