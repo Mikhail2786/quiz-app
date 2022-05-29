23b072d5472869e090edcdabb37fcd577debd8ba
Mikhail <mikhail86@icloud.com>
2022-05-29 15:20:49 +0100

    functions replaced for lodash shuffle to fix bug

--- assets/jscript/quiz.js
+++ assets/jscript/quiz.js
@@ -41,72 +41,72 @@ const questions = [
     answer: 2
   },
 
-  // {
-  //   question: "Lauryn Hill is an amazing singer and rapper, but which group did she start her career with?", 
-  //   choices: ["Fugees", "De La Soul", "The Firm", "The Diplomats"], 
-  //   answer: 0
-  // },
+  {
+    question: "Lauryn Hill is an amazing singer and rapper, but which group did she start her career with?", 
+    choices: ["Fugees", "De La Soul", "The Firm", "The Diplomats"], 
+    answer: 0
+  },
 
-  // {
-  //   question: "The Notorious B.I.G discovered one of the greatest female rappers to ever grace a mic, but what is her name?", 
-  //   choices: ["Foxy Brown", "Eve", "Lil Kim", "Amil"],      
-  //   answer: 2
-  // },
+  {
+    question: "The Notorious B.I.G discovered one of the greatest female rappers to ever grace a mic, but what is her name?", 
+    choices: ["Foxy Brown", "Eve", "Lil Kim", "Amil"],      
+    answer: 2
+  },
 
-  // {
-  //   question: "Jay-Z, Dame Dash and one other founded Roc-A-Fella Records, who is the third founder?", 
-  //   choices: ["P Diddy", "Irv Gotti", "Kareem 'Biggs' Burke", "Top Dawg"],    
-  //   answer: 2
-  // },
+  {
+    question: "Jay-Z, Dame Dash and one other founded Roc-A-Fella Records, who is the third founder?", 
+    choices: ["P Diddy", "Irv Gotti", "Kareem 'Biggs' Burke", "Top Dawg"],    
+    answer: 2
+  },
 
-  // {
-  //   question: "What is the name of Nipsey Hussle's first mixtape?", 
-  //   choices: ["The Marathon Continues", "Slauson Boys 2", "Mailbox Money", "Bullets Ain't Got No Name"],
-  //   answer: 3
-  // },
+  {
+    question: "What is the name of Nipsey Hussle's first mixtape?", 
+    choices: ["The Marathon Continues", "Slauson Boys 2", "Mailbox Money", "Bullets Ain't Got No Name"],
+    answer: 3
+  },
 
-  // {
-  //   question: "What is the Name of of J Coles second album?",
-  //   choices: ["Cole World: The Sideline Story", "Forest Hills Drive", "Friday Night lights", "Born Sinner"],
-  //   answer: 3
-  // },
+  {
+    question: "What is the Name of of J Coles second album?",
+    choices: ["Cole World: The Sideline Story", "Forest Hills Drive", "Friday Night lights", "Born Sinner"],
+    answer: 3
+  },
 
-  // {
-  //   question: "What is the name of the sub-genre made famous by Dr Dre & Snoop Dogg?", 
-  //   choices: ["C-Funk","G-Funk","B-Funk", "P-Funk"],
-  //   answer: 1
-  // },
+  {
+    question: "What is the name of the sub-genre made famous by Dr Dre & Snoop Dogg?", 
+    choices: ["C-Funk","G-Funk","B-Funk", "P-Funk"],
+    answer: 1
+  },
 
-  // {
-  //   question: "Where drill music originate from?", 
-  //   choices: ["New York", "London", "Paris", "Chicago"],
-  //   answer: 3
-  // },
-  // {
-  //   question: "What is the name of the collective Future started with?", 
-  //   choices: ["Native Tongue’s", "The Dungeon Family", "Rich Gang", "Digital Underground"],
-  //   answer: 1
-  // },
-  // {
-  //   question: "What is Nipsey Hussle’s slogan?", 
-  //   choices: ["The marathon keeps going", "The marathon Doesn’t stops", "The marathon continues", "The marathon forever"],
-  //   answer: 2
-  // },
-  // {
-  //   question: "Hip Hop’s founding father is Dj Kool Hurc, but which country was he born?", 
-  //   choices: ["The United States Of America", "England", "Nigeria", "Jamaica"],
-  //   answer: 3
-  // },
-  // {
-  //   question: "Giggs undoubtedly made the biggest uk hip hop song with “Talking the hardest”, but who made the original song?", 
-  //   choices: ["Stat Quo", "Bank Roll Fresh", "Rich Boy", "Shawty Low"],
-  //   answer: 0
-  // },
-  // {
-  //   question: "What is the name of UK rapper Dave’s debut album?", 
-  //   choices: ["Psychodrama", "We’re All Alone In This Together", "Survival", "Game Over "],
-  //   answer: 0
-  // }
+  {
+    question: "Where drill music originate from?", 
+    choices: ["New York", "London", "Paris", "Chicago"],
+    answer: 3
+  },
+  {
+    question: "What is the name of the collective Future started with?", 
+    choices: ["Native Tongue’s", "The Dungeon Family", "Rich Gang", "Digital Underground"],
+    answer: 1
+  },
+  {
+    question: "What is Nipsey Hussle’s slogan?", 
+    choices: ["The marathon keeps going", "The marathon Doesn’t stops", "The marathon continues", "The marathon forever"],
+    answer: 2
+  },
+  {
+    question: "Hip Hop’s founding father is Dj Kool Hurc, but which country was he born?", 
+    choices: ["The United States Of America", "England", "Nigeria", "Jamaica"],
+    answer: 3
+  },
+  {
+    question: "Giggs undoubtedly made the biggest uk hip hop song with “Talking the hardest”, but who made the original song?", 
+    choices: ["Stat Quo", "Bank Roll Fresh", "Rich Boy", "Shawty Low"],
+    answer: 0
+  },
+  {
+    question: "What is the name of UK rapper Dave’s debut album?", 
+    choices: ["Psychodrama", "We’re All Alone In This Together", "Survival", "Game Over "],
+    answer: 0
+  }
 ];
 
 const shuffledQuestions = shuffle(questions); 
