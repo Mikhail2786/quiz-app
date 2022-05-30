7e33ca5fe67136ec2a433eae5b8c75c8f2f46e89
Mikhail <mikhail86@icloud.com>
2022-05-30 16:03:07 +0100

    added a div with id final-score and a show score function

--- assets/jscript/quiz.js
+++ assets/jscript/quiz.js
@@ -35,78 +35,78 @@ const questions = [
     answer: 3
   },
 
-  // {
-  //   question: "Ante Up remix was a hit song released in 2000 featuring Busta Rhymes and Remy Martin, but who are the duo who released the song?", 
-  //   choices: ["C-N-N", "Mobb Deep", "M.O.P", "Method Man & Re]dman"],
-  //   answer: 2
-  // },
+  {
+    question: "Ante Up remix was a hit song released in 2000 featuring Busta Rhymes and Remy Martin, but who are the duo who released the song?", 
+    choices: ["C-N-N", "Mobb Deep", "M.O.P", "Method Man & Re]dman"],
+    answer: 2
+  },
 
-  // {
-  //   question: "Lauryn Hill is an amazing singer and rapper, but which group did she start her career with?", 
-  //   choices: ["Fugees", "De La Soul", "The Firm", "The Diplomats"], 
-  //   answer: 0
-  // },
+  {
+    question: "Lauryn Hill is an amazing singer and rapper, but which group did she start her career with?", 
+    choices: ["Fugees", "De La Soul", "The Firm", "The Diplomats"], 
+    answer: 0
+  },
 
-  // {
-  //   question: "The Notorious B.I.G discovered one of the greatest female rappers to ever grace a mic, but what is her name?", 
-  //   choices: ["Foxy Brown", "Eve", "Lil Kim", "Amil"],      
-  //   answer: 2
-  // },
+  {
+    question: "The Notorious B.I.G discovered one of the greatest female rappers to ever grace a mic, but what is her name?", 
+    choices: ["Foxy Brown", "Eve", "Lil Kim", "Amil"],      
+    answer: 2
+  },
 
-  // {
-  //   question: "Jay-Z, Dame Dash and one other founded Roc-A-Fella Records, who is the third founder?", 
-  //   choices: ["P Diddy", "Irv Gotti", "Kareem 'Biggs' Burke", "Top Dawg"],    
-  //   answer: 2
-  // },
+  {
+    question: "Jay-Z, Dame Dash and one other founded Roc-A-Fella Records, who is the third founder?", 
+    choices: ["P Diddy", "Irv Gotti", "Kareem 'Biggs' Burke", "Top Dawg"],    
+    answer: 2
+  },
 
-  // {
-  //   question: "What is the name of Nipsey Hussle's first mixtape?", 
-  //   choices: ["The Marathon Continues", "Slauson Boys 2", "Mailbox Money", "Bullets Ain't Got No Name"],
-  //   answer: 3
-  // },
+  {
+    question: "What is the name of Nipsey Hussle's first mixtape?", 
+    choices: ["The Marathon Continues", "Slauson Boys 2", "Mailbox Money", "Bullets Ain't Got No Name"],
+    answer: 3
+  },
 
-  // {
-  //   question: "What is the Name of of J Coles second album?",
-  //   choices: ["Cole World: The Sideline Story", "Forest Hills Drive", "Friday Night lights", "Born Sinner"],
-  //   answer: 3
-  // },
+  {
+    question: "What is the Name of of J Coles second album?",
+    choices: ["Cole World: The Sideline Story", "Forest Hills Drive", "Friday Night lights", "Born Sinner"],
+    answer: 3
+  },
 
-  // {
-  //   question: "What is the name of the sub-genre made famous by Dr Dre & Snoop Dogg?", 
-  //   choices: ["C-Funk","G-Funk","B-Funk", "P-Funk"],
-  //   answer: 1
-  // },
+  {
+    question: "What is the name of the sub-genre made famous by Dr Dre & Snoop Dogg?", 
+    choices: ["C-Funk","G-Funk","B-Funk", "P-Funk"],
+    answer: 1
+  },
 
-  // {
-  //   question: "Where drill music originate from?", 
-  //   choices: ["New York", "London", "Paris", "Chicago"],
-  //   answer: 3
-  // },
-  // {
-  //   question: "What is the name of the collective Future started with?", 
-  //   choices: ["Native Tongue’s", "The Dungeon Family", "Rich Gang", "Digital Underground"],
-  //   answer: 1
-  // },
-  // {
-  //   question: "What is Nipsey Hussle’s slogan?", 
-  //   choices: ["The marathon keeps going", "The marathon Doesn’t stops", "The marathon continues", "The marathon forever"],
-  //   answer: 2
-  // },
-  // {
-  //   question: "Hip Hop’s founding father is Dj Kool Hurc, but which country was he born?", 
-  //   choices: ["The United States Of America", "England", "Nigeria", "Jamaica"],
-  //   answer: 3
-  // },
-  // {
-  //   question: "Giggs undoubtedly made the biggest uk hip hop song with “Talking the hardest”, but who made the original song?", 
-  //   choices: ["Stat Quo", "Bank Roll Fresh", "Rich Boy", "Shawty Low"],
-  //   answer: 0
-  // },
-  // {
-  //   question: "What is the name of UK rapper Dave’s debut album?", 
-  //   choices: ["Psychodrama", "We’re All Alone In This Together", "Survival", "Game Over "],
-  //   answer: 0
-  // }
+  {
+    question: "Where drill music originate from?", 
+    choices: ["New York", "London", "Paris", "Chicago"],
+    answer: 3
+  },
+  {
+    question: "What is the name of the collective Future started with?", 
+    choices: ["Native Tongue’s", "The Dungeon Family", "Rich Gang", "Digital Underground"],
+    answer: 1
+  },
+  {
+    question: "What is Nipsey Hussle’s slogan?", 
+    choices: ["The marathon keeps going", "The marathon Doesn’t stops", "The marathon continues", "The marathon forever"],
+    answer: 2
+  },
+  {
+    question: "Hip Hop’s founding father is Dj Kool Hurc, but which country was he born?", 
+    choices: ["The United States Of America", "England", "Nigeria", "Jamaica"],
+    answer: 3
+  },
+  {
+    question: "Giggs undoubtedly made the biggest uk hip hop song with “Talking the hardest”, but who made the original song?", 
+    choices: ["Stat Quo", "Bank Roll Fresh", "Rich Boy", "Shawty Low"],
+    answer: 0
+  },
+  {
+    question: "What is the name of UK rapper Dave’s debut album?", 
+    choices: ["Psychodrama", "We’re All Alone In This Together", "Survival", "Game Over "],
+    answer: 0
+  }
 ];
 
 const shuffledQuestions = shuffle(questions); 
@@ -206,7 +206,18 @@ const clearAll = () => {
   }
 }
 
-// create function that checks if a question has been answered before moving to the next question
+const showScore = () => {
+  if (gameState.score < 5) {
+    console.log(`Wack!!! ${userNameInput.value}, you only scored ${gameState.score} out of ${maxQuestion}. i know you can do better. Try again!!`)
+  } else if (gameState.score > 10) {
+    console.log(`You smahesd it ${userNameInput.value}!!! You scored ${gameState.score} out of ${maxQuestion}. Ask you friends if they can do better.`)
+  } else {
+    console.log(`You done aite ${userNameInput.value}. You scored ${gameState.score} out of ${maxQuestion}, You can do better, try again.`)
+  }
+  console.log(`your score is ${gameState.score}`)
+  //store div with id="final-score" in a variable called finalScore (use getElementByID)
+  //1.create score message starting with an empty string
+}
 // When called hides the home and  section and shows the quiz section in the browser.
 const playQuiz = () => {
   home.classList.add("hidden");
@@ -220,6 +231,7 @@ const getResults = () => {
   home.classList.add("hidden")
   quiz.classList.add("hidden")
   results.classList.remove("hidden")
+  showScore();
 }
 
 // This function runs an if statement to determine which of the three functions should be called based on what button is clicked, 
